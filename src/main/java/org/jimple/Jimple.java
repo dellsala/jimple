ca169635923a468ca0c4a381b907a621970bc1ce
zuker <lndkuzmin@gmail.com>
2013-01-31 23:25:33 +0200

    updated copyright

--- src/main/java/org/jimple/Jimple.java
+++ src/main/java/org/jimple/Jimple.java
@@ -2,7 +2,7 @@ package org.jimple;
 /*
  * This file is part of Jimple.
  *
- * Copyright (c) 2013 Kuzmin Leonid
+ * Copyright (c) 2013 Leonid Kuzmin
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
